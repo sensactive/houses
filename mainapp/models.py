529ab9666f1bbddb5ae58145cb0b389481edde33
sensactive <sensor14@list.ru>
2018-12-01 21:55:31 +0300

    created models and refactoring templates for new models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True, verbose_name='Категория')
+    info = models.TextField(verbose_name='Описание категории')
+    price = models.PositiveIntegerField(verbose_name='Цена от')
+    img = models.ImageField(upload_to='categories/%Y/%m/%d', verbose_name='Картинка категории')
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=32, unique=True, verbose_name='Название проекта')
+    info = models.TextField(verbose_name='Описание проекта')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    img = models.ImageField(upload_to='products/%Y/%m/%d', verbose_name='Картинка проекта')
+    category = models.ForeignKey(Category, on_delete=models.SET('NULL'), null=True)
+
+    def __str__(self):
+        return self.name
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,25 +1,21 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
 def main_view(request):
-    typesItem = [
-        {'name': 'aspen', 'src': 'images/types/aspen.jpg'},
-        {'name': 'heaven', 'src': 'images/types/heaven.jpg'},
-        {'name': 'veil', 'src': 'images/types/veil.jpg'},
-        {'name': 'tower', 'src': 'images/types/tower.jpg'},
-
-    ]
-    return render(request, 'mainapp/index.html',  {'typeItems': typesItem})
+    typesItem = Category.objects.all()
+    len_items = len(typesItem)
+    return render(request, 'mainapp/index.html',  {'typeItems': typesItem, 'len_items': len_items})
 
 def projects_view(request):
-    typesItem = [
-        {'name': 'aspen', 'src': 'images/types/aspen.jpg', 'price': '300000'},
-        {'name': 'heaven', 'src': 'images/types/heaven.jpg', 'price': '350000'},
-        {'name': 'veil', 'src': 'images/types/veil.jpg', 'price': '400000'},
-        {'name': 'tower', 'src': 'images/types/tower.jpg', 'price': '450000'},
-
-    ]
-    return render(request, 'mainapp/projects.html', {'items': typesItem})
+    typesItem = Category.objects.all()
+    len_items = len(typesItem)
+    return render(request, 'mainapp/projects.html', {'items': typesItem, 'len_items': len_items})
 
 def contacts_view(request):
     return render(request, 'mainapp/contacts.html')
+
+def products_view(request):
+    category = Category.objects.first()
+    products = Product.objects.all()
+    return render(request, 'mainapp/product.html', {'products': products, 'qw': category})
